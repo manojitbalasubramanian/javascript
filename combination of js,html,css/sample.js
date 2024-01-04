743bc97a9cac4aecec37ac400ec37babbf247a70
manojitbalasubramanian <manojitbalasubramanianr@gmail.com>
2024-01-04 07:36:36 +0530

    js in html

---
+++ combination of js,html,css/sample.js
@@ -0,0 +1,12 @@
+
+var asd= document.getElementById("idname")
+
+console.log(asd.innerText)
+
+asd.innerText="hello world!"
+let message="hello"
+//asd.innerHTML="<p>HELLO</p>"
+
+asd.innerHTML=`<h1><p><b>${message}</b></p></h1>`
+
+asd.style.backgroundColor="black"
